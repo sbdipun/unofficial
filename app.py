df21befc223f2b2fe71b0cab9b2723efa4f3deb2
sbdipun <87560288+sbdipun@users.noreply.github.com>
2025-01-18 19:44:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -98,5 +98,4 @@ if __name__ == "__main__":
     import hypercorn.asyncio
     import asyncio
     config = hypercorn.Config()
-    config.bind = ["0.0.0.0:8000"]
     asyncio.run(hypercorn.asyncio.serve(app, config))
